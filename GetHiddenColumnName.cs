b7affcefbae0a926fc81f8d0a53445d0ffbde13c
doug0102 <shea.wow@hotmail.com>
2025-02-05 14:42:06 -0500

    New function added to get the hidden column names

---
+++ GetHiddenColumnName.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+
+namespace appsvc_function_dev_cm_listmgmt_dotnet001
+{
+    public class GetHiddenColumnName
+    {
+        private readonly ILogger<GetHiddenColumnName> _logger;
+
+        public GetHiddenColumnName(ILogger<GetHiddenColumnName> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("GetHiddenColumnName")]
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+        {
+            _logger.LogInformation("GetHiddenColumnName received a request.");
+
+            var columnNames = new List<string>();
+
+            try
+            {
+                Config config = new Config();
+                GraphServiceClient client = Common.GetClient(_logger);
+
+                var columns = await client
+                    .Sites[config.SiteId]
+                    .Lists[config.ListId]
+                    .Columns
+                    .GetAsync(requestConfiguration => 
+                    {
+                        requestConfiguration.QueryParameters.Select = ["id", "name", "hidden", "displayName"];
+                    });
+
+                if (columns?.Value != null)
+                {
+                    foreach (var column in columns.Value)
+                    {
+                        if (column.DisplayName == "JobType_0" || column.DisplayName == "ProgramArea_0")
+                        {
+                            var columnName = $"{column.DisplayName} - {column.Name}";
+
+                            columnNames.Add(columnName);
+
+                            _logger.LogInformation(columnName);
+
+                            if (columnNames.Count == 2)
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                if (e.InnerException is not null) _logger.LogError(e.InnerException.Message);
+                _logger.LogError(e.StackTrace);
+
+                return new BadRequestResult();
+            }
+
+            return new OkObjectResult(string.Join(", ", columnNames));
+        }
+    }
+}
