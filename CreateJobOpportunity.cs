a18f0a8baeee926d3efb1bfe96edd5c3022e198e
Stéphanie Lefebvre <STLEFEBV@tbs-sct.gc.ca>
2025-02-21 12:54:49 -0500

    function anonymous

--- CreateJobOpportunity.cs
+++ CreateJobOpportunity.cs
@@ -16,7 +16,7 @@ namespace appsvc_function_dev_cm_listmgmt_dotnet001
         }
 
         [Function("CreateJobOpportunity")]
-        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequest req)
+        public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequest req)
         {
             _logger.LogInformation("CreateJobOpportunity received a request.");
 
